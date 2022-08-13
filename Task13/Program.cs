509e4d3f2fb83f8b42b372471064aaf21cb615f5
Maxim <sibmaks.khv@mail.ru>
2022-08-13 21:16:08 +1000

    Add 2 Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -6,32 +6,14 @@
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number < 99)
-{
-    Console.Write("В веденном числе нет третьей цифры");
-}    
-if (number > 99 & number <1000)
-{
-    int A = number % 10;
-    Console.Write($"Третья цифра числа: {A}");
-}
-if (number > 999 & number <10000)
-{
-    int A = (number % 100 - number % 10) / 10;
-    Console.Write($"Третья цифра числа: {A}");
-}
-if (number > 9999 & number <100000)
-{
-    int A = (number % 1000 - number % 100) / 100;
-    Console.Write($"Третья цифра числа: {A}");
-}
-if (number > 99999 & number <1000000)
-{
-    int A = (number % 10000 - number % 1000) / 1000;
-    Console.Write($"Третья цифра числа: {A}");
-}
-if (number > 999999 & number <10000000)
-{
-    int A = (number % 100000 - number % 10000) / 10000;
-    Console.Write($"Третья цифра числа: {A}");
-}
+// 
+
+string resoult = number.ToString();
+int [] array = new int[resoult.Length];
+int i = 0;
+
+
+Console.WriteLine($"Третья цифра числа: {resoult[2]}");
+
+
+
